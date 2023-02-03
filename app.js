e1a695fb82095c4b5d33fce3dfd398f64bc15e3d
Thiago Bohana <98626454+thiagobohana@users.noreply.github.com>
2023-02-03 10:06:20 -0300

    console log indicando modificacoes de thiago

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O thiago fez esse commit sem voce ver na aula!")
 
